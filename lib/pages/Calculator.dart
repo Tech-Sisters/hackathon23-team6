4e72e5f596e77581fd8bea704e0d22acf5410a83
Fiza Khan <fiza1892003@gmail.com>
2023-11-26 19:43:56 +0530

    created calculator page

--- lib/pages/Calculator.dart
+++ lib/pages/Calculator.dart
@@ -6,8 +6,122 @@ class CalculatorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String hexColor = "#2E86AB";
+     String hexColor1 = "#85C1E9";
     return Scaffold(
-      backgroundColor: HexColor(hexColor)
+      backgroundColor: HexColor(hexColor),
+      body: Center(
+        child : Column(
+          children: [
+            SizedBox(height: 30),
+            Text("Enter your annual income :",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+              ),),
+            SizedBox(height : 20),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                
+                
+              ),
+            ),
+             SizedBox(height: 30),
+            Text("Enter the amount of gold & silver (in grams):",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+              ),),
+            SizedBox(height : 20),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                
+                
+              ),
+            ),
+             SizedBox(height: 30),
+            Text("Enter the value of any stock or investment you own : ",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+              ),),
+            SizedBox(height : 20),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                
+                
+              ),
+            ),
+             SizedBox(height: 30),
+            Text("Enter the value of personal and living expenses:",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+              ),),
+            SizedBox(height : 20),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                
+                
+              ),
+            ),
+             SizedBox(height: 30),
+            Text("Enter the amount of debts you owe:",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+              ),),
+            SizedBox(height : 20),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                
+                
+              ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(onPressed: (){
+              print("button pressed");
+              
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: HexColor(hexColor1),
+              padding: EdgeInsets.symmetric(horizontal: 36, vertical: 20),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18)
+              )
+            ),
+             child: Text("Calculate",
+             style: TextStyle(
+              fontSize: 18,
+             ),)),
+
+          ],
+        )
+        ),
     );
   }
 }
